fcbf00ad478ee327348ba91718e1f680711f8f11
LaurMerl <l.merlo4@gmail.com>
2017-11-18 18:56:03 +0100

    Raw frontend interface
    Signed-off-by: LaurMerl <l.merlo4@gmail.com>

--- app/src/main/java/org/prodscan/MainPage.kt
+++ app/src/main/java/org/prodscan/MainPage.kt
@@ -1,12 +1,21 @@
 package org.prodscan
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+        val button_acquisition = findViewById<Button>(R.id.acqButton);
+
+        button_acquisition.setOnClickListener {
+            val intent = Intent(this, ScanPage::class.java)
+            startActivity(intent);
+        }
     }
 }
