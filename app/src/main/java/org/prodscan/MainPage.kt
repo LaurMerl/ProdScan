c79a19d1598be53a82027926fbf13c08f74a7705
Marco Bedulli <marco.bedulli@lunatech.com>
2017-11-21 21:35:51 +0100

    Add list views

--- app/src/main/java/org/prodscan/MainPage.kt
+++ app/src/main/java/org/prodscan/MainPage.kt
@@ -4,15 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.util.Log
 import org.prodscan.database.InitDatabase
-import org.prodscan.database.model.Acquisition
-import org.prodscan.database.model.Product
-import org.prodscan.database.model.Scan
-import org.prodscan.database.model.State
-import org.prodscan.database.tools.DBHelper
-import org.prodscan.database.tools.Database
-import java.util.*
 
 class MainPage : AppCompatActivity() {
 
@@ -21,12 +13,17 @@ class MainPage : AppCompatActivity() {
         setContentView(R.layout.activity_main_page)
 
         val button_acquisition = findViewById<Button>(R.id.acqButton);
+        val button_acquisitions = findViewById<Button>(R.id.button_acquisitions);
 
         button_acquisition.setOnClickListener {
             val intent = Intent(this, ScanPage::class.java)
             startActivity(intent);
         }
 
+        button_acquisitions.setOnClickListener {
+            val intent = Intent(this, AcquisitionListPage::class.java)
+            startActivity(intent);
+        }
         // Initialisation on the DB
         InitDatabase(this)
     }
